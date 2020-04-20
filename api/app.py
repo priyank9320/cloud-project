7b51e65b990ab4a9632ddf256c0ef1e75fd2d150
Priyank <priyank.m9320@gmail.com>
2020-04-20 20:21:33 +0100

    cleaned code

---
+++ api/app.py
@@ -0,0 +1,115 @@
+#pip install autocorrect
+# pip install google-cloud-translate
+from flask import Flask, request, render_template, jsonify
+from autocorrect import Speller
+import wordninja
+import time
+from google.cloud import translate_v2 as translate
+import mysql.connector
+import os
+import json
+
+
+
+
+
+
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./GoogleAPICredentials.json"
+
+db_connection = mysql.connector.connect(host=os.environ["DB_HOST"], database=os.environ["DB_NAME"], user=os.environ["DB_USER"], password=os.environ["DB_PASSWORD"])
+
+insert_query = """INSERT INTO cloud_project.preprocess (id, job, original_text, processed_text)
+                           VALUES
+                           ('{}', '{}', '{}', '{}') """
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/api/spell', methods=['POST','GET'])
+def spell():
+    if request.method == 'POST':
+        id = request.form.get('id')
+        id = str(id) + '-' + time.asctime()
+        original_text=request.form.get('text')
+        spell = Speller()
+        corrected_text = spell(original_text)
+        query=insert_query.format(id,'spell',original_text,corrected_text)
+        cursor = db_connection.cursor()
+        cursor.execute(query)
+        db_connection.commit()
+        cursor.close()
+
+        return '''<h3> the corrected text is: {}, (the id is: {} )</h3>'''.format(corrected_text,id)
+
+
+    return '''<form method = 'POST'>
+    id <input type="text" name="id">
+    text <input type="text" name="text">
+    <input type="submit">
+    </form>'''
+
+
+
+@app.route('/api/space', methods=['POST','GET'])
+def space():
+    if request.method == 'POST':
+        id = request.form.get('id')
+        id = str(id) + '-' + time.asctime()
+        original_text=request.form.get('text')
+
+
+        split_text = wordninja.split(request.form.get('text'))
+        corrected_text = str(' '.join(split_text))
+
+        query=insert_query.format(id,'space',original_text,corrected_text)
+        cursor = db_connection.cursor()
+        cursor.execute(query)
+        db_connection.commit()
+        cursor.close()
+
+
+        return '''<h3> the corrected text is: {}, (the id is: {} )</h3>'''.format(corrected_text,id)
+
+
+    return '''<form method = 'POST'>
+    id <input type="text" name="id">
+    text <input type="text" name="text">
+    <input type="submit">
+    </form>'''
+
+
+
+@app.route('/api/translate', methods=['POST','GET'])
+def translate_text():
+    translator  = translate.Client()
+    if request.method == 'POST':
+        id = request.form.get('id')
+        id = str(id) + '-' + time.asctime()
+        original_text=request.form.get('text')
+
+        corrected_text = translator.translate(request.form.get('text'),target_language="en")['translatedText']
+
+        query=insert_query.format(id,'translate',original_text,corrected_text)
+        cursor = db_connection.cursor()
+        cursor.execute(query)
+        db_connection.commit()
+        cursor.close()
+
+        return '''<h3> the corrected text is: {}, (the id is: {})</h3>'''.format(corrected_text,id)
+
+
+    return '''<form method = 'POST'>
+    id <input type="text" name="id">
+    text <input type="text" name="text">
+    <input type="submit">
+    </form>'''
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(threaded=True,host='0.0.0.0',debug=True,port=8088)
--- home/home.py
+++ home/home.py
@@ -1,10 +1,34 @@
 # home.py
-from flask import Flask
+from flask import Flask, request, render_template, jsonify
+from autocorrect import Speller
+import wordninja
+import time
+import mysql.connector
+import os
+import json
+
+
+
+
+
+
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Home page"
+@app.route('/home', methods=['GET'])
+def display_table():
+    db_connection = mysql.connector.connect(host=os.environ["DB_HOST"], database=os.environ["DB_NAME"], user=os.environ["DB_USER"], password=os.environ["DB_PASSWORD"])
+    query="select * from cloud_project.preprocess;"
+    cursor = db_connection.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    db_connection.close()
+
+    return render_template("home.html",value=rows)
+
+
 
 if __name__ == '__main__':
     app.run(threaded=True,host='0.0.0.0',port=5000)
